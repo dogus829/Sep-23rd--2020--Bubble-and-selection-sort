9e8f7a8641afe5a0fbf52b145332ead67281f6d2
dogus829 <68568704+dogus829@users.noreply.github.com>
2020-09-23 12:36:12 -0700

    Update selectionsort.js

--- selectionsort.js
+++ selectionsort.js
@@ -9,11 +9,11 @@ function selectionSort(arr) {
 
     for (var i = 0; i < arr.length; i++) {
 
-        let min = i; //  storing the index of minimum element
+        let min = i;
 
         for (var j = i + 1; j < arr.length; j++) {
             if (arr[min] > arr[j]) {
-                min = j; // updating the index of minimum element
+                min = j; 
             }
         }
 
